9ce746935a63f8b2f72f741181f5d57cb7ecfedd
Evgenii Morozov <evmorov@gmail.com>
2021-09-05 22:49:56 +0200

    Add cors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors')
 const morgan = require('morgan');
 
 const app = express();
 app.use(express.json());
-
+app.use(cors())
 app.use(
   morgan((tokens, req, res) => {
     const method = tokens.method(req, res);
