bcee8f9b25e8fa0ef755b3704d96a59bbe9eebf2
Evgenii Morozov <evmorov@gmail.com>
2021-09-18 23:16:09 +0200

    3.14-3.18

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(
       tokens['response-time'](req, res),
       'ms',
     ];
-    if (method === 'POST') {
+    if (['POST', 'PUT'].includes(method)) {
       format.push(JSON.stringify(req.body));
     }
     return format.join(' ');
@@ -33,14 +33,26 @@ app.get('/api/persons', (request, response) => {
   });
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    if (person) {
-      response.json(person);
-    } else {
-      response.status(404).end();
-    }
-  });
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const { body } = request;
+
+  Person.findByIdAndUpdate(request.params.id, { number: body.number }, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.post('/api/persons', (request, response) => {
@@ -64,23 +76,41 @@ app.post('/api/persons', (request, response) => {
   });
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-  Person.findByIdAndDelete(request.params.id, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
       response.status(204).end();
-    }
-  });
+    })
+    .catch((error) => next(error));
 });
 
 app.get('/info', (request, response) => {
-  response.send(`
-    <div>Phonebook has info for ${persons.length} people</div>
-    <br>
-    <div>${Date()}</div>
-  `);
+  Person.count().then((count) => {
+    response.send(`
+      <div>Phonebook has info for ${count} people</div>
+      <br>
+      <div>${Date()}</div>
+    `);
+  });
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
